16896a54f3a005dc3f454b9124ae40bf3caf3045
ErickSchuenck <esfmokdeci@gmail.com>
2022-11-01 10:17:36 -0300

    feat cron task

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,22 @@ import express from "express";
 import "express-async-errors";
 import cors from "cors";
 import dotenv from "dotenv";
-dotenv.config();
-
 import router from "./routers/router";
 import errorHandler from "./middlewares/errorHandler";
+
+dotenv.config();
+
+const cron = require("node-cron");
+cron.schedule("* * * * * *", () => {
+  console.log("Every Second");
+});
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
 app.use(errorHandler);
+
+console.log;
 
 export default app;
